b6c4fd2f403a302e206a1be194930d302564fec1
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-16 17:16:35 +0100

    update Area test & js

---
+++ src/js/KataPadding.js
@@ -0,0 +1,14 @@
+const getPaddingString =(givenString,givenFinalLentgh,fillingElement)=>{
+    debugger;
+     if (givenString.lentgh === givenFinalLentgh){
+         return givenString;
+     } else if (givenString.lentgh >= givenFinalLentgh){
+        const givenLetter = givenString.split('');
+        for (let i = 0; i < givenString.length; i++) {
+            const element = array[i];
+            
+        }
+     }
+};
+
+export default getPaddingString;
---
+++ src/js/KataPadding.test.js
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom/extend-expect";
+import { prettyDOM } from "@testing-library/dom";
+import { render, screen } from "@testing-library/react";
+import kata from "./KataPadding";
+
+describe("Testing KataPading component",()=>{
+    test("paddingLeft('hola', 6, 'x') devuelve 'xxhola'", ()=>{
+        const result = kata('hola', 6, 'x');
+        console.log(result);
+        expect(result).toBe('xxhola');
+    });
+
+
+
+
+/* 
+paddingLeft('hola', 6, 'a') devuelve 'aahola'
+paddingLeft('ee', 4, 'aa') devuelve 'aaee'
+paddingLeft('xxxx', 6, 'x') devuelve 'xxxxxx'
+paddingLeft('', 6, 'x') devuelve 'xxxxxx'
+paddingLeft('hola mi amigo', 6, 'x') devuelve 'hola mi amigo'
+paddingLeft('xxxx', 0, 'x') devuelve 'xxxx' */
+
+});
