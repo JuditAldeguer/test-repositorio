29ff8a00146e2d91c5915ca75b582408b782c027
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-16 16:07:16 +0100

    update Area test & js

--- src/js/Area.test.js
+++ src/js/Area.test.js
@@ -1,19 +1,16 @@
 /* Al testear un fichero de JavaScript (y no un componente de React), no es necesario importar import { render, screen } from '@testing-library/react';. */
 import "@testing-library/jest-dom/extend-expect";
-import { prettyDOM } from "@testing-library/dom";
 import Area from "./Area";
 
 describe("Testing Area component", ()=> {
     test("getSquareArea(3) equals 9", ()=> {
         const result = Area.getSquareArea(3);
-        console.log(prettyDOM(result));
+        console.log(result);
         expect(result).toBe(9);
     });
     test("getSquareTriangle(3,4) equals 6", ()=>{
-        const result = (3*4)/2;
-        console.log(prettyDOM(result));
+        const result = Area.getSquareArea(3,4);
+        console.log(result);
         expect(result).toBe(6);
     });
 });
-  
-/* Crea otro test que compruebe que si le pasamos a la función getSquareTriangle() un 3 y un 4 esta nos devuelve un 6. */
