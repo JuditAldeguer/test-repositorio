b795e116d5dad9aee5817f94a2e80054b2a1503a
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-16 16:12:03 +0100

    update Area test & js

--- src/js/Area.js
+++ src/js/Area.js
@@ -4,8 +4,8 @@ const getSquareArea = (c) => {
 };
 getSquareArea();
 
-const getSquareTriangle =(b,a)=>{
-
+const getSquareTriangle =(b,h)=>{
+   return (b*h)/2;
 };
 getSquareTriangle();
 /*  Este fichero debe tener otra función llamada  que recibe un número con la base y otro con la altura del triángulo y retorna el área del mismo.
